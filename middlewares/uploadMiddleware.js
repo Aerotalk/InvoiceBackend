2cc3a6d6be47325e97de33c7c666ba43a41b20d7
pro123sub <pro123sub@gmail.com>
2026-06-03 00:16:59 +0530

    us-east-1

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -5,11 +5,12 @@ const path = require('path');
 
 const s3 = new S3Client({
     endpoint: process.env.S3_ENDPOINT,
-    region: process.env.S3_REGION,
+    region: process.env.S3_REGION === 'auto' ? 'us-east-1' : process.env.S3_REGION,
     credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY_ID,
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
-    }
+    },
+    forcePathStyle: true
 });
 
 const storage = multerS3({
